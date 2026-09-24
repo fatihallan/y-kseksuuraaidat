6984be6da99a6376b77f52d4b67e291fe696d769
fatihallan <fatihallan31@gmail.com>
2026-09-24 21:41:00 +0300

    feat: Dijital Şûra V1 ana sayfa ve üye girişi

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"Dijital Şûra",description:"Yüksek Şûra dijital topluluk platformu"};
+export default function RootLayout({children}){return <html lang="tr"><body>{children}</body></html>}
